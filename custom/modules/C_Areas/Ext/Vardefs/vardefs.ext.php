e6a9fb2ee582eb0fc34456f2221ee92cbc3584dd
hoc.buicao <hoc.buicao@sugarcrm.com.vn>
2012-06-16 04:42:18 +0000

    Resolve issue

--- custom/modules/C_Areas/Ext/Vardefs/vardefs.ext.php
+++ custom/modules/C_Areas/Ext/Vardefs/vardefs.ext.php
@@ -32,4 +32,19 @@ $dictionary["C_Areas"]["fields"]["c_areas_cobbabuntries_ida"] = array (
   'vname' => 'LBL_C_AREAS_COUNTRIES_FROM_C_AREAS_TITLE',
 );
 
+
+// created: 2012-03-02 11:57:23
+$dictionary["C_Areas"]["fields"]["c_areas_destinations"] = array (
+  'name' => 'c_areas_destinations',
+  'type' => 'link',
+  'relationship' => 'c_areas_destinations',
+  'source' => 'non-db',
+  'side' => 'right',
+  'vname' => 'LBL_C_AREAS_DESTINATIONS_FROM_DESTINATIONS_TITLE',
+);
+
+
+ // created: 2012-03-05 13:21:03
+
+ 
 ?>
