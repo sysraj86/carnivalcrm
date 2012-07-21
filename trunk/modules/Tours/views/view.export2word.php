3bee40afa8bad9b2175abaf89550bdd063506239
hoc.buicao@sugarcrm.com.vn <hoc.buicao@sugarcrm.com.vn@77ba39bc-ca8a-8046-9ce4-1e4b15790ef8>
2012-07-21 03:06:02 +0000

    [Hieu] Resolve issue 1438

--- trunk/modules/Tours/views/view.export2word.php
+++ trunk/modules/Tours/views/view.export2word.php
@@ -28,7 +28,39 @@
             $template = str_replace('${TRANSPORT}',$tour->transport2, $template);
             $template = str_replace('${START_DATE}',$tour->start_date, $template);
             $template = str_replace('${DURATION}',$tour->duration, $template);
-            $template = str_replace('${PICTURE}',$sugar_config['site_url']."/modules/images/".$tour->picture, $template);
+            
+            // Hieu fix issue 1438
+            if($tour->picture){
+                $main_picture = '
+                    <!--[if gte vml 1]>
+                    <o:wrapblock>
+                        <v:shape id="Picture_x0020_11"
+                                 o:spid="_x0000_s1028" type="#_x0000_t75"
+                                 alt=""
+                                 style=\'position:absolute;margin-left:0;margin-top:0;width:470.55pt;height:234pt;
+                  z-index:251657216;visibility:visible;mso-wrap-style:square;
+                  mso-width-percent:0;mso-height-percent:0;mso-wrap-distance-left:9pt;
+                  mso-wrap-distance-top:0;mso-wrap-distance-right:9pt;
+                  mso-wrap-distance-bottom:0;mso-position-horizontal:center;
+                  mso-position-horizontal-relative:margin;mso-position-vertical:absolute;
+                  mso-position-vertical-relative:text;mso-width-percent:0;mso-height-percent:0;
+                  mso-width-relative:page;mso-height-relative:page\'>
+                            <v:imagedata src="'.$sugar_config['site_url']."/modules/images/".$tour->picture.'"
+                                         o:title="phan%20thiet%20beach"/>
+                            <o:lock v:ext="edit" aspectratio="f"/>
+                            <w:wrap type="topAndBottom" anchorx="margin"/>
+                        </v:shape><![endif]--><![if !vml]><img width=627 height=312
+                                                               src="'.$sugar_config['site_url']."/modules/images/".$tour->picture.'"
+                                                               alt=""
+                                                               v:shapes="Picture_x0020_11"><![endif]><!--[if gte vml 1]></o:wrapblock>
+                    <![endif]-->
+                ';   
+            }else{
+                $main_picture = '';
+            }
+            $template = str_replace('${PICTURE}', $main_picture, $template);
+            // End issue 1438
+            
             $template = str_replace('${TOUR_PROGRAM_LINES}',html_entity_decode_utf8($tour->get_data_to_expor2word()),$template);
             $size=strlen($template);
             $filename = "TOUR_PROGRAM_".$tour->name.".doc";
