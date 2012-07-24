310f1f633b37339da94cac204c7866d9dac364d4
hoc.buicao <hoc.buicao@sugarcrm.com.vn>
2012-07-24 03:38:13 +0000

    [Hieu] Resolve issue 1289

--- custom/modules/Destinations/language/en_us.lang.php
+++ custom/modules/Destinations/language/en_us.lang.php
@@ -1,10 +1,11 @@
 <?php
-// created: 2012-03-14 10:43:48
+// created: 2012-07-24 11:19:16
 $mod_strings = array (
   '' => 'Image',
   'LBL_PICTURE2' => 'Picture2',
   'LBL_LIST_NAME' => 'Name',
   'LBL_NAME' => 'Name',
   'LBL_AREA' => 'Area',
+  'LBL_CITY_PROVINCE' => 'City / Province',
 );
 ?>
