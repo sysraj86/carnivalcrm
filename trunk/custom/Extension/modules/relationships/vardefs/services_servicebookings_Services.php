96cdd78a03e0c56f97536c0b7fe81e8072d1fa4f
hoc.buicao@sugarcrm.com.vn <hoc.buicao@sugarcrm.com.vn@77ba39bc-ca8a-8046-9ce4-1e4b15790ef8>
2012-08-06 07:58:42 +0000

    [Hieu] Fix issue 1280

---
+++ trunk/custom/Extension/modules/relationships/vardefs/services_servicebookings_Services.php
@@ -0,0 +1,10 @@
+<?php
+// created: 2012-08-06 15:50:49
+$dictionary["Services"]["fields"]["services_servicebookings"] = array (
+  'name' => 'services_servicebookings',
+  'type' => 'link',
+  'relationship' => 'services_servicebookings',
+  'source' => 'non-db',
+  'side' => 'right',
+  'vname' => 'LBL_SERVICES_SERVICEBOOKINGS_FROM_SERVICEBOOKINGS_TITLE',
+);
--- trunk/custom/modules/ServiceBookings/Ext/Vardefs/vardefs.ext.php
+++ trunk/custom/modules/ServiceBookings/Ext/Vardefs/vardefs.ext.php
@@ -76,4 +76,35 @@ $dictionary["ServiceBookings"]["fields"]["restaurant96e9aurants_ida"] = array (
   'vname' => 'LBL_RESTAURANTS_SERVICEBOOKINGS_FROM_SERVICEBOOKINGS_TITLE',
 );
 
+
+// created: 2012-08-06 15:50:49
+$dictionary["ServiceBookings"]["fields"]["services_servicebookings"] = array (
+  'name' => 'services_servicebookings',
+  'type' => 'link',
+  'relationship' => 'services_servicebookings',
+  'source' => 'non-db',
+  'vname' => 'LBL_SERVICES_SERVICEBOOKINGS_FROM_SERVICES_TITLE',
+);
+$dictionary["ServiceBookings"]["fields"]["services_seebookings_name"] = array (
+  'name' => 'services_seebookings_name',
+  'type' => 'relate',
+  'source' => 'non-db',
+  'vname' => 'LBL_SERVICES_SERVICEBOOKINGS_FROM_SERVICES_TITLE',
+  'save' => true,
+  'id_name' => 'services_sde2fervices_ida',
+  'link' => 'services_servicebookings',
+  'table' => 'services',
+  'module' => 'Services',
+  'rname' => 'name',
+);
+$dictionary["ServiceBookings"]["fields"]["services_sde2fervices_ida"] = array (
+  'name' => 'services_sde2fervices_ida',
+  'type' => 'link',
+  'relationship' => 'services_servicebookings',
+  'source' => 'non-db',
+  'reportable' => false,
+  'side' => 'right',
+  'vname' => 'LBL_SERVICES_SERVICEBOOKINGS_FROM_SERVICEBOOKINGS_TITLE',
+);
+
 ?>
