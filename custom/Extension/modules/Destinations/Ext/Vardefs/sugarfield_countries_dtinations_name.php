792940f53b2746ad39f56a7fd5f14f4ab976e919
hoc.buicao <hoc.buicao@sugarcrm.com.vn>
2012-08-07 04:27:43 +0000

    [Hieu] Fix issue 1519

--- custom/Extension/modules/Destinations/Ext/Vardefs/sugarfield_countries_dtinations_name.php
+++ custom/Extension/modules/Destinations/Ext/Vardefs/sugarfield_countries_dtinations_name.php
@@ -1,5 +1,6 @@
 <?php
  // created: 2012-07-24 11:20:20
 $dictionary['Destination']['fields']['countries_dtinations_name']['massupdate']=1;
+$dictionary['Destination']['fields']['countries_dtinations_name']['importable']= true;
 
  ?>
