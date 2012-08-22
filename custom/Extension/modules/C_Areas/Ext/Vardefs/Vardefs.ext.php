bcc5058ffb625f9fe965d9d5fe6919d69fccaf0a
hoc.buicao <hoc.buicao@sugarcrm.com.vn>
2012-08-22 10:21:43 +0000

    [Hieu] Resolve issue 1563

--- custom/Extension/modules/C_Areas/Ext/Vardefs/Vardefs.ext.php
+++ custom/Extension/modules/C_Areas/Ext/Vardefs/Vardefs.ext.php
@@ -1,10 +1,12 @@
 <?php
  // created: 2012-03-05 13:21:03
+/* fix issue 1563
 $dictionary['C_Areas']['indices'][] = array(
     'name' =>'idx_areas_name', 
     'type' =>'index', 
     'fields'=>array('name')
 );
+*/
 
 $dictionary['C_Areas']['indices'][] = array(
     'name' =>'idx_areas_code', 
--- custom/modules/C_Areas/Ext/Vardefs/vardefs.ext.php
+++ custom/modules/C_Areas/Ext/Vardefs/vardefs.ext.php
@@ -27,11 +27,13 @@ $dictionary["C_Areas"]["fields"]["c_areas_destinations"] = array (
  
 
  // created: 2012-03-05 13:21:03
+/* fix issue 1563
 $dictionary['C_Areas']['indices'][] = array(
     'name' =>'idx_areas_name', 
     'type' =>'index', 
     'fields'=>array('name')
 );
+*/
 
 $dictionary['C_Areas']['indices'][] = array(
     'name' =>'idx_areas_code', 
