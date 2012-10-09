b5994a1fff3b578e8483cfd8bcae5f9c85b6e0f2
hoc.buicao@sugarcrm.com.vn <hoc.buicao@sugarcrm.com.vn@77ba39bc-ca8a-8046-9ce4-1e4b15790ef8>
2012-10-09 07:58:53 +0000

    [Hieu] fix bug 1266

--- trunk/custom/modules/GroupPrograms/logic_hooks.php
+++ trunk/custom/modules/GroupPrograms/logic_hooks.php
@@ -5,6 +5,8 @@
     $hook_array['before_save'][] = Array(1,'updateDatetime', 'custom/modules/GroupPrograms/GroupProgramLogicHooks.php','GroupProgramLogicHooks', 'updateDatetime');  
     $hook_array['before_save'][] = Array(2,'updatechild2', 'custom/modules/GroupPrograms/GroupProgramLogicHooks.php','GroupProgramLogicHooks', 'updatechild2');  
     $hook_array['before_save'][] = Array(3,'updatechildefrom2to12', 'custom/modules/GroupPrograms/GroupProgramLogicHooks.php','GroupProgramLogicHooks', 'updatechildefrom2to12');  
-    $hook_array['before_save'][] = Array(4,'updateAudlts', 'custom/modules/GroupPrograms/GroupProgramLogicHooks.php','GroupProgramLogicHooks', 'updateAudlts');  
-    $hook_array['before_save'][] = Array(2,'getcountofcus', 'custom/modules/GroupPrograms/GroupProgramLogicHooks.php','GroupProgramLogicHooks', 'getcountofcus');  
+    $hook_array['before_save'][] = Array(4,'updateAudlts', 'custom/modules/GroupPrograms/GroupProgramLogicHooks.php','GroupProgramLogicHooks', 'updateAudlts');
+    
+    $hook_array['process_record'] = Array();   
+    $hook_array['process_record'][] = Array(1,'getNumOfCus', 'custom/modules/GroupPrograms/GroupProgramLogicHooks.php','GroupProgramLogicHooks', 'getNumOfCus');  
 ?>
