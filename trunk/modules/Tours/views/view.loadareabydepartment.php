29e3850dbd76e8e80d9affd384cf9fa94eed2a39
hoc.buicao@sugarcrm.com.vn <hoc.buicao@sugarcrm.com.vn@77ba39bc-ca8a-8046-9ce4-1e4b15790ef8>
2012-10-23 10:09:13 +0000

    [Hieu] Fix lỗi không hiển thị đúng Area khi edit

--- trunk/modules/Tours/views/view.loadareabydepartment.php
+++ trunk/modules/Tours/views/view.loadareabydepartment.php
@@ -12,12 +12,17 @@
 
             global $db;
             $focus = new Tour();
+            $focus->retrieve($_GET['record']);
             if(isset($_POST['department']) && isset($_POST['department']) != ''){
                 $data = $focus->loadAreaByDepartment($_POST['department']);
                 if(count($data)>0){
                     $area_options .= '<option value ="">none</option>';
                     foreach($data as $row){
-                        $area_options .= "<option data-code='" . $row['code'] . "' data-country='" . $row['country_id'] . "' value='" . $row['id'] . "'>" . $row['name'].'-'.$row['country']."</option>";       
+                        $selected = '';
+                        if($focus->area == $row['id']){
+                            $selected = 'selected="selected"';    
+                        }
+                        $area_options .= "<option data-code='" . $row['code'] . "' data-country='" . $row['country_id'] . "' value='" . $row['id'] . "' ".$selected.">" . $row['name'].'-'.$row['country']."</option>";       
                     }
                     echo $area_options;    
                 }
