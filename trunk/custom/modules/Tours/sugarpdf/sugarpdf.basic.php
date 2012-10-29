88cb522770737f22e41087e810ec2b9ad62cfc77
hieu.nguyen@sugarcrm.com.vn <hieu.nguyen@sugarcrm.com.vn@77ba39bc-ca8a-8046-9ce4-1e4b15790ef8>
2012-10-29 03:30:15 +0000

    Xử lý export tour full frame ra pdf

--- trunk/custom/modules/Tours/sugarpdf/sugarpdf.basic.php
+++ trunk/custom/modules/Tours/sugarpdf/sugarpdf.basic.php
@@ -12,7 +12,7 @@ class ToursSugarpdfBasic extends SugarpdfSmarty{
         $this->print_header = true;
         $this->print_footer = true;
         
-        $this->fileName = 'tour.pdf';
+        $this->fileName = 'Tour.pdf';
         $this->SetFont('freeserif','',8);
         
         // Khai bao duong dan den file tpl
@@ -21,8 +21,29 @@ class ToursSugarpdfBasic extends SugarpdfSmarty{
         // Thiet lap header
         $this->setHeaderData("company_logo.png", "50", "Tours Details", "Export from SugarCRM");
 
-        // Assign data sang tpl
-        $this->ss->assign("NAME", $this->bean->name);
+        // Assign data sang tpl        
+        $record = isset($_GET["record"]) ? htmlspecialchars($_GET["record"]) : '';
+        $tour = new Tour();
+        $tour->retrieve($record);
+        
+        $this->ss->assign('NAME', $this->bean->name);
+        $this->ss->assign('TOUR_NOTE',html_entity_decode_utf8($tour->description));
+        $this->ss->assign('CODE',$tour->tour_code);
+        $this->ss->assign('TRANSPORT',$tour->transport2);
+        $this->ss->assign('START_DATE',$tour->start_date);
+        $this->ss->assign('DURATION',$tour->duration);
+        
+        if($tour->picture && file_exists($sugar_config['site_url']."/modules/images/".$tour->picture)){
+            $main_picture = '<img width="627" height="312" src="'.$sugar_config['site_url']."/modules/images/".$tour->picture.'" >';   
+        }else{
+            $main_picture = '';
+        }
+        
+        $this->ss->assign('PICTURE', $main_picture);        
+        $this->ss->assign('TOUR_PROGRAM_LINES',html_entity_decode_utf8($tour->get_data_to_export2pdf()));
+        
+        // Cleanup cache file
+        ob_clean();
         /*$this->ss->assign("PHONE_OFFICE", $this->bean->phone_office);
         $this->ss->assign("WEBSITE", $this->bean->website);
         $this->ss->assign("PHONE_FAX", $this->bean->phone_fax);
