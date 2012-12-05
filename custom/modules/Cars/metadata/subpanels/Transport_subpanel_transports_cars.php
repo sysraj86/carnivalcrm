7e05d8475d6b321f61974dd868cfd6e6069ae6de
trang.le <trang.le@sugarcrm.com.vn>
2012-12-05 08:29:35 +0000

    ẩn cột transport name cho subpanel car trong module transport.

--- custom/modules/Cars/metadata/subpanels/Transport_subpanel_transports_cars.php
+++ custom/modules/Cars/metadata/subpanels/Transport_subpanel_transports_cars.php
@@ -1,5 +1,5 @@
 <?php
-// created: 2012-09-26 11:38:45
+// created: 2012-12-05 15:27:15
 $subpanel_layout['list_fields'] = array (
   'driver_name' => 
   array (
@@ -20,14 +20,6 @@ $subpanel_layout['list_fields'] = array (
   array (
     'type' => 'int',
     'vname' => 'LBL_NUMOFSEAT',
-    'width' => '10%',
-    'default' => true,
-  ),
-  'transport_name' => 
-  array (
-    'type' => 'relate',
-    'studio' => 'visible',
-    'vname' => 'LBL_TRANSPORT_NAME',
     'width' => '10%',
     'default' => true,
   ),
